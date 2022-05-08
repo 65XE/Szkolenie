e1f688edcbc67d7e4d8ce04842252819013b8ac4
65XE <pksoftware.pl@gmail.com>
2022-05-08 12:11:38 +0200

    validation under construction

--- validation.py
+++ validation.py
@@ -1,18 +1,62 @@
+def debug():
+    x = "54-117"
+    y = x[2:3:]
+
+    if x[2:3:] != '-':
+        print(f"To NIE jest -> {x[2:3:]} <-")
+    if x[2:3:] == '-':
+        print(f"To jest -> {x[2:3:]} <-")
+
+def validateName(name):
+    if not name.isalpha():
+        return False
+    return True
+
+def validateAge(age):
+    if not age.isnumeric():
+        return False
+    if int(age) <= 0:
+        return False
+    return True
+
+def validatePasswd(passwd):
+    if len(passwd) == 0:
+        return False
+    return True
+
+def validateZipCode(zip):
+    if not (zip[0:2:].isdecimal() and zip[3:5:].isdecimal()) and zip[2:3:] != '-' and len(zip) != 6:
+        return False
+    return True
+
 def validate():
+    name = ''
+    age = ''
+    passwd = ''
+    zip = ''
     while True:
-        imie = input("Podaj imię : ")
-        if not imie.istitle() and not imie.isalpha():
-            break
-        print(imie)
+        # name = input("Podaj imię : ")
+        # if not validateName(name):
+        #     break
+        #
+        # age = input("Podaj wiek : ")
+        # if not validateAge(age):
+        #     break
+        #
+        # passwd = input("Podaj hasło : ")
+        # if not validatePasswd(passwd):
+        #     break
 
-        age = input("Podaj nwiek : ")
-        if not age.isdecimal() and age <= 0 and age.:
+        zip = input("Podaj kod pocztowy : ")
+        if not validateZipCode(zip):
             break
-        print(age)
 
+        print(f"* {name}\n* {age}\n* {passwd}\n* {zip}")
+
+debug()
 validate()
 
-#Name
-#Age
-#Password
-#Zip code
+# Name
+# Age
+# Password
+# Zip code
