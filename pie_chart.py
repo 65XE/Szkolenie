278b16c9b1caa46302bc46a6fd4bd063cd72addd
Pawel Kowalczyk <pawel.kowalczyk.ext@nokia.com>
2022-05-24 15:09:20 +0200

    pie_chart underr construction

--- pie_chart.py
+++ pie_chart.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import re
 
 def przyklad_z_czatu():
     path = "C:\\Windows"
@@ -19,6 +20,65 @@ def przyklad_z_czatu():
     print(F"Rozmiar plików to {round(total_size * (10 ** -6))} megabajtów.")
 
 
+def return_number_in_tags(tag, line):
+        if tag in line:
+            s = line
+            res = s.find(">")
+            s = s.replace(s[:res + 1], "")
+            res = s.find("<")
+            a = s.replace(s[res:], "")
+            return int(a)
+
+
+def exception_count(line):
+    return return_number_in_tags("<Exceptions>", line)
+
+def passed_count(line):
+    return return_number_in_tags("<Passed>", line)
+
+def failures_count(line):
+    return return_number_in_tags("<Failures>", line)
+
+def proba():
+    path = "C:\\WORK\\prywatne\\nauka\\Szkolenie Python\\Task"
+
+    passed = "<Passed>"
+    failures = "<Failures>"
+    exceptions = "<Exceptions>"
+    passed_no = 0
+    failures_no = 0
+    exceptions_no = 0
+    total_files = 0
+
+    for dir_path, _, files in os.walk(path):
+        for name in files:
+            if not name.startswith("LOG_") or not name.endswith(".xml"):
+                continue
+            new_path = dir_path + "\\" + name
+            file1 = open(new_path, "r", encoding='UTF8')
+            for line in file1:
+                if passed in line:
+                    passed_no += passed_count(line)
+                if failures in line:
+                    failures_no += failures_count(line)
+                if exceptions in line:
+                    exceptions_no += exception_count(line)
+
+
+            total_files += 1
+
+    print(F"Liczba plików .xml to {total_files}")
+    print(f"Liczba Pass : {passed_no}")
+    print(f"Liczba Faili : {failures_no}")
+    print(f"Liczba Exceptionów : {exceptions_no}")
+
+    y = np.array([passed_no, failures_no, exceptions_no])
+
+    plt.pie(y)
+    plt.show()
+    #print(F"Rozmiar plików to {total_size} bajtów.")
+    #print(F"Rozmiar plików to {round(total_size * (10 ** -6))} megabajtów.")
+
 if __name__ == '__main__':
     print(f"Poczatek")
 
@@ -27,6 +87,6 @@ if __name__ == '__main__':
     # plt.pie(y)
     # plt.show()
 
-    przyklad_z_czatu()
+    proba()
 
     print(f"Koniec")
