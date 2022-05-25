deabc31543dc59142c4f80edba0fffcca7ec5a05
65XE <pksoftware.pl@gmail.com>
2022-05-25 20:38:30 +0200

    Method Factory Added

--- report_generator.py
+++ report_generator.py
@@ -1,44 +1,50 @@
 import csv
 import json
 
-class ReportGenerator:
-    def __init__(self, data):
-        self.__data = data
 
-    def __create_dict(self):
+class Report:
+    def __init__(self, data):
+        self.data = data
+
+
+class ReportGenerator:
+    def __create_dict(self, data):
         keys = ('Passed', 'Failures', 'Exceptions')
-        values = (self.__data[0], self.__data[1], self.__data[2])
+        values = (data[0], data[1], data[2])
         return dict(zip(keys, values))
 
-    def __json_generator(self):
-        data = self.__create_dict()
+    def __json_generator(self, data):
+        dict = self.__create_dict(data)
 
         with open('report.json', 'wt', encoding='UTF8') as fout:
-            json.dump(data, fout)
+            json.dump(dict, fout)
 
-
-    def __csv_generator(self):
+    def __csv_generator(self, data):
         header = ['Passed', 'Failures', 'Exceptions']
 
         with open('report.csv', 'wt', encoding='UTF8') as fout:
             csvout = csv.writer(fout)
             csvout.writerow(header)
-            csvout.writerow(self.__data)
+            csvout.writerow(data)
+
+    def __both_generators(self, data):
+        self.__json_generator(data)
+        self.__csv_generator(data)
 
     def __choose_format(self, format):
         match format:
             case "1":
-                self.__json_generator()
+                return self.__json_generator
             case "2":
-                self.__csv_generator()
+                return self.__csv_generator
             case "3":
-                self.__json_generator()
-                self.__csv_generator()
+                return self.__both_generators
             case _:
                 print("This is not 1 or 2. There will be no reports.")
 
-    def generate(self):
+    def generate(self, report):
         print(f"Hi,\nPlease specify in which file format you want to generate file?\n"
               f"Press 1 for JSON\nPress 2 for CSV\nPress 3 for both")
         format = input("Please enter the number : ")
-        self.__choose_format(format)
+        generator = self.__choose_format(format)
+        return generator(report)
--- reports.py
+++ reports.py
@@ -8,5 +8,8 @@ if __name__ == '__main__':
     tests_counter.print_summary()
 
     #generate report
-    report = ReportGenerator(tests_counter.get_data_for_report())
-    report.generate()
+    # report = ReportGenerator(tests_counter.get_data_for_report())
+    # report.generate()
+    report = tests_counter.get_data_for_report()
+    report_generator = ReportGenerator()
+    report_generator.generate(report)
